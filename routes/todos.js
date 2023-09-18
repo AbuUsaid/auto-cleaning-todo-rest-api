ebadafe77d9c90432ed90aca3dc775264dd394b3
AbuUsaid <usaidabu321@gmail.com>
2023-09-18 20:02:06 +0530

    implement a post route

--- routes/todos.js
+++ routes/todos.js
@@ -2,9 +2,30 @@ import express from 'express';
 
 const router = express.Router(); //initialising router, just as we used app in index.js
 
+const todos = [
+  {
+    title: 'Finish report',
+    description: 'Complete the project report.',
+    completed: false,
+  },
+  {
+    title: 'Grocery shopping',
+    description: 'Buy groceries for the week.',
+    completed: true,
+  },
+];
+
 //all routes in here are starting with /todos
 router.get('/', (req, res) => {
-  res.send('Hello');
+  res.send(todos);
+});
+
+router.post('/', (req, res) => {
+  const todo = req.body;
+
+  todos.push(todo);
+
+  res.send(`Todo with the title " ${todo.title} " added to the database!`);
 });
 
 export default router;
