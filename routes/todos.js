12794825bb934004365cb19205aed761517c1151
AbuUsaid <usaidabu321@gmail.com>
2023-09-18 21:44:58 +0530

    implement delete route which takes id from req.params and deletes the specific todo

--- routes/todos.js
+++ routes/todos.js
@@ -3,7 +3,7 @@ import { v4 as uuidv4 } from 'uuid';
 
 const router = express.Router(); //initialising router, just as we used app in index.js
 
-const todos = [];
+let todos = [];
 
 //all routes in here are starting with /todos
 router.get('/', (req, res) => {
@@ -15,7 +15,7 @@ router.post('/', (req, res) => {
 
   todos.push({ ...todo, id: uuidv4() });
 
-  res.send(`Todo with the title " ${todo.title} " added to the database!`);
+  res.send(`Todo with the title "${todo.title}" added to the database!`);
 });
 
 router.get('/:id', (req, res) => {
@@ -24,6 +24,14 @@ router.get('/:id', (req, res) => {
   const foundTodo = todos.find((todo) => todo.id === id);
 
   res.send(foundTodo);
+});
+
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+
+  todos = todos.filter((todo) => todo.id !== id);
+
+  res.send(`Todo with the id "${id}" deleted from the database.`);
 });
 
 export default router;
